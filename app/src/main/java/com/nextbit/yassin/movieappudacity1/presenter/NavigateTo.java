63631cfc4c2aa8ffa964b1e22456142edfc37de9
yassinelhadedy <yassinelhadedy@hotmail.com>
2019-04-20 17:21:50 +0200

    refactor movie view

---
+++ app/src/main/java/com/nextbit/yassin/movieappudacity1/presenter/NavigateTo.java
@@ -0,0 +1,7 @@
+package com.nextbit.yassin.movieappudacity1.presenter;
+
+import com.nextbit.yassin.movieappudacity1.domain.model.Movie;
+
+public interface NavigateTo {
+    void navigateTo(Movie movie);
+}
